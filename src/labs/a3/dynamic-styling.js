1a24cba4006bad786d1ddc909a6707b74a2f7dfb
MihirKapile <mihirkapile@gmail.com>
2023-07-22 18:14:01 -0700

    Javascript and React.js Assignment 3

---
+++ src/labs/a3/dynamic-styling.js
@@ -0,0 +1,12 @@
+import Classes from "./classes"
+import Styles from "./styles"
+function DynamicStyling(){
+    return(
+        <>
+            <Styles/>
+            <Classes/>
+        </>
+    )
+
+}
+export default DynamicStyling
